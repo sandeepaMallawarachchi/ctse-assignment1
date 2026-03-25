c2de6cefb7f8455945049c57e27cd6d3ebd3f976
TasH-i <hansaneet@gmail.com>
2026-03-25 10:23:50 +0530

    create cors validations

--- backend/auth-service/src/main/java/com/ctse/auth_service/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/ctse/auth_service/config/SecurityConfig.java
@@ -16,6 +16,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+// cors validations
 @Configuration
 @EnableConfigurationProperties(JwtProperties.class)
 public class SecurityConfig {
